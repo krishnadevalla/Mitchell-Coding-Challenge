148775be0a62537525d2a3d5d636b3a5162bde0f
Leela Krishna Devalla <krishnadevalla@outlook.com>
2018-04-16 01:29:59 -0400

    Final commit

---
+++ MitchellWebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using MitchellClassLib;
+using System.Web.Http;
+using Unity;
+using Unity.Lifetime;
+
+namespace MitchellWebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Web API routes
+
+            // Unity for dependency injection
+            var container = new UnityContainer();
+            container.RegisterType<IRepository, VehiclesRepository>(new HierarchicalLifetimeManager());
+            config.DependencyResolver = new UnityResolver(container);
+
+            config.EnableCors();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
